6ca33e6df8afb776e49ecd019728c2cc0f7215b4
HandrewOltenish <148466747+HandrewOltenish@users.noreply.github.com>
2025-01-30 23:57:17 +0100

    Added squash_the_creeps_3d_mobcollision updates + gif

--- Squash the Creeps 3D/player.gd
+++ Squash the Creeps 3D/player.gd
@@ -4,7 +4,8 @@ extends CharacterBody3D
 @export var speed = 14
 # The downward acceleration when in the air, in meters per second squared
 @export var fall_acceleration = 75
-
+# Vertical impulse applied to character upon jumping in meters per second.
+@export var jump_impulse = 20
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
@@ -20,9 +21,8 @@ func _physics_process(delta):
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
-	if Input.is_action_pressed("jump"):
-		if is_on_floor():
-			target_velocity.y = direction.y + (fall_acceleration * delta)
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
 # In 3D the XZ plane is the ground plane.
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
