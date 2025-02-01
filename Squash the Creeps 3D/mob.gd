5fd014f0460ca097736693765abe085893e2807b
HandrewOltenish <148466747+HandrewOltenish@users.noreply.github.com>
2025-02-01 16:21:06 +0100

    Uploaded squash_the_creeps_3d_playerdie.gif + death mechanic

--- Squash the Creeps 3D/mob.gd
+++ Squash the Creeps 3D/mob.gd
@@ -1,22 +1,29 @@
 extends CharacterBody3D
-signal squashed
+
 # Minimum speed of the mob in meters per second
 @export var min_speed = 10
 # Maximum speed of the mob in meters per second
 @export var max_speed = 18
 
+# Emitted when the player jumped on the mob
+signal squashed
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
 # This function will be called from the main scene.
 func initialize(start_position, player_position):
+	# Ignore y-axis to keep the mob flat
+	var flat_player_position = player_position
+	
+	# Set the player's y position to the same as the mob's y position.
+	flat_player_position.y = start_position.y
 	# We position the mob by placing it a the start_position
 	# and rotate it towards player_position, so it looks at the player.
-	look_at_from_position(start_position, player_position, Vector3.UP)
+	look_at_from_position(start_position, flat_player_position, Vector3.UP)
 	# Rotate this mob randomly within range of -45 and +45 degrees,
 	# so that it doesn't move directly towards the player.
 	rotate_y(randf_range(-PI / 4, PI / 4))
-	# We calculate random speed (integer)
+	# We calculate random speed (integer) 
 	var random_speed = randi_range(min_speed, max_speed)
 	# We calculate a forward velocity tat represents the speed.
 	velocity = Vector3.FORWARD * random_speed
@@ -27,6 +34,6 @@ func initialize(start_position, player_position):
 
 func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
 	queue_free()
-func suqash():
+func squash():
 	squashed.emit()
 	queue_free()
