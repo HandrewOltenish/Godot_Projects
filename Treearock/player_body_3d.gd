3c88f9dbdb4b0681a21d8d6c30f3b825562a82bc
HandrewOltenish <148466747+HandrewOltenish@users.noreply.github.com>
2025-03-23 22:45:12 +0100

    Updated scripts and scenes

--- Treearock/player_body_3d.gd
+++ Treearock/player_body_3d.gd
@@ -2,35 +2,30 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
+const ACCEL = 4.0
+var CAM_SPEED = SPEED / 50
 const JUMP_VELOCITY = 4.5
-
-signal oops
+@onready var player = get_node(".")
+signal cut
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	var input_dir := Input.get_vector("Move Left", "Move Right", "Move Up", "Move Down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	var inputDir = Input.get_vector("Move Left", "Move Right", "Move Up", "Move Down")
+	if Input.is_action_pressed("Move Left"):
+		$Camera3D.rotation.y += CAM_SPEED
+	if Input.is_action_pressed("Move Right"):
+		$Camera3D.rotation.y -= CAM_SPEED
+	if Input.is_action_pressed("Move Up"):
+		Vector3($Camera3D.rotation.x, $Camera3D.rotation.y, $Camera3D.rotation.z)
+	var CameraDir = Vector3(inputDir.x, 0.0, inputDir.y).rotated(Vector3.UP, $Camera3D.rotation.y)
+	if CameraDir:
+		velocity.x = CameraDir.x * SPEED
+		velocity.z = CameraDir.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
-	
-func kaboom():
-	oops.emit()
+func cut_tree():
+	cut.emit()
 	queue_free()
-
-
-#func _on_area_3d_area_entered(area: Area3D) -> void:
-#	check
