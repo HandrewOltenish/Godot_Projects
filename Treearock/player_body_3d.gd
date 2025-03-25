dda9e035eb13d83b3fdacbf493cf6dee390b69a7
HandrewOltenish <148466747+HandrewOltenish@users.noreply.github.com>
2025-03-25 23:58:50 +0100

    Debugging velocity

--- Treearock/player_body_3d.gd
+++ Treearock/player_body_3d.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const ACCEL = 4.0
-var CAM_SPEED = SPEED / 50
+var CAM_SPEED = SPEED / 250
 const JUMP_VELOCITY = 4.5
 @onready var player = get_node(".")
 signal cut
@@ -14,17 +14,17 @@ func _physics_process(delta: float) -> void:
 	var inputDir = Input.get_vector("Move Left", "Move Right", "Move Up", "Move Down")
 	if Input.is_action_pressed("Move Left"):
 		$Camera3D.rotation.y += CAM_SPEED
+		velocity.x -= velocity.x * SPEED
+		velocity.z -= velocity.z * SPEED
 	if Input.is_action_pressed("Move Right"):
 		$Camera3D.rotation.y -= CAM_SPEED
-	if Input.is_action_pressed("Move Up"):
-		Vector3($Camera3D.rotation.x, $Camera3D.rotation.y, $Camera3D.rotation.z)
-	var CameraDir = Vector3(inputDir.x, 0.0, inputDir.y).rotated(Vector3.UP, $Camera3D.rotation.y)
+	var CameraDir := Vector3(inputDir.x, 0.0, inputDir.y).rotated(Vector3.UP, $Camera3D.rotation.y)
 	if CameraDir:
 		velocity.x = CameraDir.x * SPEED
 		velocity.z = CameraDir.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(CameraDir.x, 0, SPEED)
+		velocity.z = move_toward(CameraDir.z, 0, SPEED)
 	move_and_slide()
 func cut_tree():
 	cut.emit()
