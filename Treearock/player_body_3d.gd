709588c484c43e9f703dacfa0a9f1c5158f540ad
HandrewOltenish <148466747+HandrewOltenish@users.noreply.github.com>
2025-03-31 23:01:56 +0200

    Updated player script

--- Treearock/player_body_3d.gd
+++ Treearock/player_body_3d.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const ACCEL = 4.0
-var CAM_SPEED = SPEED / 250
+const CAM_SPEED = SPEED / 200
 const JUMP_VELOCITY = 4.5
 @onready var player = get_node(".")
 signal cut
@@ -12,19 +12,28 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	var inputDir = Input.get_vector("Move Left", "Move Right", "Move Up", "Move Down")
-	var CameraDir := Vector3(inputDir.x, 0.0, inputDir.y).rotated(Vector3.UP, $Camera3D.rotation.y)
+	var CameraDir = Vector3(inputDir.x, 0, inputDir.y).rotated(Vector3.UP, $Camera3D.rotation.y)
 	if CameraDir:
-		velocity.x += velocity.x * SPEED
-		velocity.z += velocity.z * SPEED
+		velocity.x = CameraDir.x * SPEED
+		velocity.z = CameraDir.z * SPEED
 	else:
-		velocity.x += move_toward(velocity.x, 0, SPEED)
-		velocity.z += move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(CameraDir.x, 0, SPEED)
+		velocity.z = move_toward(CameraDir.z, 0, SPEED)
 	if Input.is_action_pressed("Move Left"):
-		$Camera3D.rotation.y += CAM_SPEED
+			$Camera3D.rotation.y += CAM_SPEED
+			velocity.x = 0
+			velocity.z = 0
 	if Input.is_action_pressed("Move Right"):
-		$Camera3D.rotation.y -= CAM_SPEED
-
+			$Camera3D.rotation.y += -CAM_SPEED
+			velocity.x = 0
+			velocity.z = 0
+	#print("Player X: ",$".".position.x)
+	#print("Player Z: ",$".".position.z)
 	move_and_slide()
 func cut_tree():
 	cut.emit()
 	queue_free()
+
+
+func _on_rock_body_3d_mine_rock() -> void:
+	pass # Replace with function body.
